c7d7a0074c679a66b6b487f3b2c2b1b8bdf67e32
Achyuth Sreenath Haresamudram <achyuth.works@gmail.com>
2026-02-12 01:47:09 +0530

    Project Structure ready, venv set, MongoDB set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Achyuth',
+    author_email='achyuth.works@gmail.com',
+    packages=find_packages()
+)
