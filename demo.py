72e27c1df273a0273ab5b2de2962565c8af228f0
Achyuth Sreenath Haresamudram <achyuth.works@gmail.com>
2026-02-15 14:27:59 +0530

    Model Training Done

--- demo.py
+++ demo.py
@@ -1,11 +1,12 @@
-# -----------------------------------------
+# _______________________________________
 # for LOGGING:
 
 # from src.logger import logging
 # logging.debug("this is a demo debug")
-# ---------------------------------------
+# _______________________________________
 
-# ---------------------------------------
+
+# _______________________________________
 # for EXCEPTION:
 
 # from src.logger import logging
@@ -16,12 +17,20 @@
 # except Exception as e:
 #     logging.info(e)
 #     raise MyException(e, sys) from e
-# ---------------------------------------
+# _______________________________________
 
-# ---------------------------------------------------------------
-# for DATA INGESTION:
+
+# _________________________________________________________________
+# for DATA INGESTION, VALIDATION, TRANSFORMATION, MODEL TRAINING:
 
 from src.pipline.training_pipeline import TrainPipeline
 pipeline = TrainPipeline()
 pipeline.run_pipeline()
-# ---------------------------------------------------------------
+# _________________________________________________________________
+
+
+# mongodb+srv://Achyuth_mlops_user:Iu6mo7qIJmqRnfAU@cluster0.72vxxxt.mongodb.net/?appName=Cluster0
+
+# new username:  Achyuth
+# new pswd:      SUItvUHbmRqNVJdM
+# mongodb+srv://Achyuth:SUItvUHbmRqNVJdM@cluster0.72vxxxt.mongodb.net/?appName=Cluster0
--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -23,3 +23,8 @@ class ClassificationMetricArtifact:
     f1_score:float
     precision_score:float
     recall_score:float
+
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path:str 
+    metric_artifact:ClassificationMetricArtifact
